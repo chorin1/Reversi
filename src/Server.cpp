db09309cd86efbd52f1f0608c150e54fc0ee70f8
nitai halle <nitaihalle@gmail.com>
2017-12-04 08:00:26 -0800

    add server class

---
+++ src/Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nitaihalle on 12/4/17.
+//
+
+#include "Server.h"
---
+++ src/Server.h
@@ -0,0 +1,14 @@
+//
+// Created by nitaihalle on 12/4/17.
+//
+
+#ifndef REVERSI_SERVER_H
+#define REVERSI_SERVER_H
+
+
+class Server {
+
+};
+
+
+#endif //REVERSI_SERVER_H
