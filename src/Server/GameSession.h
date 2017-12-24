abc7003c64b217225c34702bfda8d55896caa708
chorin <chorin@gmail.com>
2017-12-24 21:39:41 +0200

    minor improvments to server

--- src/Server/GameSession.h
+++ src/Server/GameSession.h
@@ -5,12 +5,12 @@
 #pragma once
 class GameSession {
 public:
-    GameSession(int player1Socket) : player1Socket(player1Socket) {}
+    GameSession(int player1Socket) : player1Socket(player1Socket) {player2Socket=0;}
     GameSession(int player1Socket, int player2Socket) : player1Socket(player1Socket), player2Socket(player2Socket) {}
-    GameSession(GameSession other) {
+    GameSession(const GameSession &other) {
         player1Socket = other.player1Socket;
         player2Socket = other.player2Socket;
     }
     int player1Socket;
-    int player2Socket = 0;
+    int player2Socket;
 };
